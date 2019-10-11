27d1434d7ef8cab0821093b7d40d8b31178a233f
Sod Shirmen <sshirmen@mum.edu>
2019-10-11 01:03:22 -0500

    lab08

---
+++ Lab06/Lab6_Valid/src/main/java/com/students/contoller/TestController.java
@@ -0,0 +1,21 @@
+package com.students.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+    @RequestMapping("/source")
+    public String getSource(@RequestParam("pass") String passValue, Model model){
+        model.addAttribute("value", passValue);
+        return "redirect:/target";
+    }
+
+    @RequestMapping("/target")
+    public String getTarget(@RequestParam("value") String passValue, Model model){
+        model.addAttribute("pass", passValue);
+        return "display";
+    }
+}
---
+++ Lab08/sprnbootthymeleaf/src/main/java/edu/mum/sprnbootthymeleaf/SprnbootthymeleafApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.sprnbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprnbootthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprnbootthymeleafApplication.class, args);
+    }
+
+}
