e032b51e9247ffc31f9c6aacdab1442878733a83
Sod Shirmen <sshirmen@mum.edu>
2019-10-13 21:57:21 -0500

    lab09

---
+++ Lab09/src/main/java/mum/edu/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package mum.edu.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import mum.edu.domain.Employee;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/")
+    public String showForm() {
+        return "employee";
+    }
+
+
+}
